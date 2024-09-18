9f83445c2f919afd0f10a8bbf20a21423fe011ad
Laboratorio 01 Sede VES <Ves-Lab01@certus.edu.pe>
2024-09-18 10:05:45 -0500

    mi primera version

---
+++ js/libreria.js
@@ -0,0 +1,9 @@
+ //Muestra un mensaje
+ alert ('Buenos dias programadores');
+ function mensaje(){
+     //Bloque de codigo que realiza una actividad
+     alert('INSTITUTO CERTUS')
+ }
+ function presentar(){
+     alert('Lenguajes para el desarrollo y soporte de sistemas');
+ }
